fa15d6b9050655d1d0728eb29169661410cfbcd3
Jasmin Bouasavatdy <jasminbouasavatdy@hotmail.com>
2022-02-28 12:44:27 -0500

    added and if and and alert

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 // Assignment Code
+
+
+
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var count = Number(prompt("Please enter the amount of characters you would like in your password."));
+  console.log(count);
+  // added an alert to show how many characters between 6 and 150
+if (count < 6 || count > 150) {
+  alert("Please enter more than 6 characters less than 150.")
+}
+return;
+
+
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var special = "~!@#$%^&*()_+=./"
+  var genPassword = "";
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * letters.length);
+    genPassword += letters[random];
+  }
+
+  console.log(genPassword);
+
+  return genPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
