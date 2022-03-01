99c9ccdc44b371e37b981877934213760a74ada9
Jasmin Bouasavatdy <jasminbouasavatdy@hotmail.com>
2022-02-28 19:11:42 -0500

    character types and promp questions

--- assets/script.js
+++ assets/script.js
@@ -5,21 +5,49 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var count = Number(prompt("Please enter the amount of characters you would like in your password."));
-  console.log(count);
-  var letters= "abcdefghijklmnopqrstuvwxyz";
-  var upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var special= "~!@#$%^&*()-=+[]:;<>?";
-  var numbers= "0123456789"
+  var charSet = "";
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var special = "~!@#$%^&*()-=+[]:;<>?";
+  var numbers = "0123456789"
   var genPassword = "";
 
-if (count<8 || count > 128)
-alert("Please enter a character count between 8 - 128")
- return "";
+
+  var isLowerCaseLetters = Boolean(confirm("Do you want lowercase letters in your password?"))
+  if (isLowerCaseLetters) {
+    charSet += lower;
+  }
+
+  var isUpperCaseLetters = Boolean(confirm("Do you want uppercase letters in your password?"))
+  if (isUpperCaseLetters) {
+    charSet += upper;
+  }
+
+  var isSpecialCharacters = Boolean(confirm("Do you want special characters in your password?"))
+  if (isSpecialCharacters) {
+    charSet += special;
+  }
+
+  var isNumbers = Boolean(confirm("Do you want numbers in your password?"))
+  if (isNumbers) {
+    charSet += numbers;
+  }
+  if (charSet.length == 0) {
+    alert("You must have at least one character type selected. Please try again");
+    return "";
+  }
+
+  var count = Number(prompt("Please enter the amount of characters you would like in your password."));
+  console.log(count);
+
+  if (count < 8 || count > 128) {
+    alert("Please enter a password length of least 8 characters and no more than 128 characters.");
+    return "";
+  }
 
   for (var i = 0; i < count; i++) {
-    var random = Math.floor(Math.random() * letters.length);
-    genPassword += letters[random];
+    var random = Math.floor(Math.random() * charSet.length);
+    genPassword += charSet[random];
   }
 
   console.log(genPassword);
