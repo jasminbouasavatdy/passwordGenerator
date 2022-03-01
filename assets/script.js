78f71f2d2ca282190b4e590d2cbbe0a7e59a100e
Jasmin Bouasavatdy <jasminbouasavatdy@hotmail.com>
2022-02-28 19:39:18 -0500

    added comments to show what the thought process is

--- assets/script.js
+++ assets/script.js
@@ -12,34 +12,39 @@ function generatePassword() {
   var numbers = "0123456789"
   var genPassword = "";
 
-
+//created a boolean so when the confirm was asked the customer was able to answer
   var isLowerCaseLetters = Boolean(confirm("Do you want lowercase letters in your password?"))
   if (isLowerCaseLetters) {
     charSet += lower;
   }
+//created a boolean so when the confirm was asked the customer was able to answer
 
   var isUpperCaseLetters = Boolean(confirm("Do you want uppercase letters in your password?"))
   if (isUpperCaseLetters) {
     charSet += upper;
   }
+//created a boolean so when the confirm was asked the customer was able to answer
 
   var isSpecialCharacters = Boolean(confirm("Do you want special characters in your password?"))
   if (isSpecialCharacters) {
     charSet += special;
   }
+//created a boolean so when the confirm was asked the customer was able to answer
 
   var isNumbers = Boolean(confirm("Do you want numbers in your password?"))
   if (isNumbers) {
     charSet += numbers;
   }
+  // added an if statement so that if the user did not choose a character type and alert was shown and then had to start over.
   if (charSet.length == 0) {
     alert("You must have at least one character type selected. Please try again");
     return "";
   }
+//created a boolean so when the confirm was asked the customer was able to answer
 
   var count = Number(prompt("Please enter the amount of characters you would like in your password."));
   console.log(count);
-
+// added and aler so that if the character count was not in the right range the user would have to do so
   if (count < 8 || count > 128) {
     alert("Please enter a password length of least 8 characters and no more than 128 characters.");
     return "";
